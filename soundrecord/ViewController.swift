5c0f116f984e620f3660dc15422622b9ba5bd81e
nahive <masslany@gmail.com>
2016-02-23 21:04:40 +0100

    Initial Commit

---
+++ soundrecord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soundrecord
+//
+//  Created by Szymon Maślanka on 23/02/16.
+//  Copyright © 2016 Szymon Maślanka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
